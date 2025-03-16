8d8ecea5402998677c6505d230f0c4131bc95f0e
Iossif Benbassat <iossif123gg@gmail.com>
2025-03-16 10:49:28 +0200

    add vitest only for ts files and not hooks

---
+++ client/vitest.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: [
+      "**/*.spec.tsx",
+      "**/*.test.tsx",
+      "**/node_modules/**",
+      "**/dist/**",
+    ],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      thresholds: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+      include: ["src/**/*.ts", "src/**/*.js"],
+      exclude: [
+        "src/**/*.tsx",
+        "src/**/*.jsx",
+        "src/**/hooks/**",
+        "src/**/*.hook.ts",
+        "src/**/*.hooks.ts",
+        "src/**/use*.ts",
+      ],
+    },
+  },
+});

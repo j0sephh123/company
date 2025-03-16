843071c798230b429aa9c20ae2334affc18280a1
j0sephh123 <30373032+j0sephh123@users.noreply.github.com>
2025-03-16 08:00:39 +0000

    Deploying to gh-pages from @ j0sephh123/company@af30a7f1fe4a7925c08b377e7f734aabbdf8b1f9 🚀

--- client/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "https://github.com/j0sephh123/company",
-});

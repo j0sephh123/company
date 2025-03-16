61d1f67ae3081bcfe5dea88ce1ef835fc157f561
Iossif Benbassat <iossif123gg@gmail.com>
2025-03-16 09:47:57 +0200

    update config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://github.com/j0sephh123/company",
+  base: "/company/",
 });
